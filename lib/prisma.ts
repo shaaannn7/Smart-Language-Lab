65befe07837f5039295f51edb9ffe608afa178db
shan <shaandadhich1@gmail.com>
2026-05-08 01:52:55 +0530

    Configure project for Vercel deployment: Switch to PostgreSQL and standard Prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,7 @@
 import { PrismaClient } from "@prisma/client"
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
-import Database from "better-sqlite3"
 
 const prismaClientSingleton = () => {
-  const adapter = new PrismaBetterSqlite3({ url: "dev.db" })
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 declare global {
